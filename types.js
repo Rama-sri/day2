901e538b290ada7bd0c9bd6dd495013df865cac1
Rama-sri <97947124+Rama-sri@users.noreply.github.com>
2022-01-29 11:39:08 +0530

    Add files via upload

---
+++ types.js
@@ -0,0 +1,18 @@
+//1.Its is 'number' type in  type of integer
+console.log(typeof(1))
+//Its is 'number' type in type of folating
+console.log(typeof(1.1))
+//It is type of 'string' because value between " " or ' '
+console.log(typeof('1.1'))
+//it is type of 'boolean' only the result is true or false
+console.log(typeof(true))
+//it is the type of 'null'. it which give a wrong imperssion that
+console.log(typeof(null))
+//it is 'undefined' if the variable is not declared the result is undifined
+console.log(typeof(undefined))
+//it is the 'object' and it will take empty square bracket as object 
+console.log(typeof([]))
+//it is the 'object' and it will take empty bracket as obeject
+console.log(typeof({}))
+//its is type of NaN is a 'number'
+console.log(typeof(NaN))
